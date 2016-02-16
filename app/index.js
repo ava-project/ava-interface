8ef8872fb86f7be8e5e3bfc11e0788a03790d90f
Dorian Amouroux <dor.amouroux@gmail.com>
2016-02-16 16:54:29 +0100

    setup bower and a little bit angular

--- app/index.js
+++ app/index.js
@@ -21,7 +21,7 @@ app.on('ready', function () {
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // Open the devtools.
   // mainWindow.openDevTools();
@@ -32,6 +32,7 @@ app.on('ready', function () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
+    process.exit(0);
   });
 
 });
