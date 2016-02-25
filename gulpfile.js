da7c9e217ee2823e60cd0d85bd800ec70facb710
Dorian Amouroux <dor.amouroux@gmail.com>
2016-02-25 01:49:17 +0100

    less implementation in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@
 var gulp = require('gulp'),
     childProcess = require('child_process'),
     electron = require('electron-prebuilt');
+// less' shit
+concat = require('gulp-concat'),
+    minifyCSS = require('gulp-minify-css'),
+    less = require("gulp-less"),
+    combiner = require('stream-combiner2'),
+    path = require('path');
 
 // create the gulp task
 gulp.task('run', function() {
@@ -9,3 +15,28 @@ gulp.task('run', function() {
         stdio: 'inherit'
     });
 });
+
+var inputLess = path.join(__dirname, './app/**/*.less'),
+    destLess = path.join(__dirname, './app/assets/css');
+
+gulp.task('less', function() {
+    var combined = combiner.obj([
+        gulp.src(inputLess),
+        less(),
+        concat('dist.css'),
+        minifyCSS(),
+        gulp.dest(destLess)
+    ]);
+
+    combined.on('error', function(e) {
+        console.error(e);
+    });
+
+    return (combined);
+});
+
+gulp.task('watch', ['less'], function() {
+    gulp.watch(inputLess, ['less']);
+});
+
+gulp.task('default', ['run'], function() {});
